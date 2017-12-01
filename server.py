fb4fdaa65f561277e080a302ff48c61c8771e2c5
Paul <paulwinegard@gmail.com>
2017-12-01 12:28:18 -0800

    session added, new html file

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, session
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print "Got Post Info"
+
+    name = request.form["name"]
+    email = request.form["email"]
+    return redirect('/')
 
 @app.route('/show')
 def show_user():
